79b136dffb19fc7491247c026b3fc0c29e4b63c6
Justin Westerfield <justin.westerfield@dbb.su.se>
2023-11-04 01:09:10 +0100

    Updated calc_f_dimer with chi-squared analysis. Updated plot_probs.

--- calc_f_dimer.py
+++ calc_f_dimer.py
@@ -13,87 +13,133 @@ Section 5.2 (equation 8)
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import chi2
+from plot_probs import plot_probability_bar
 
 
-def least_squares_plot(FD, R2, Fdimer_prediction):
+def least_squares_plot(fd, r2, fd_pred):
     """
     Plots the results of least-squares analysis.
     """
-    plt.semilogy(FD, R2)
-    plt.axvline(x=Fdimer_prediction, color='red', linestyle='--')
-    inlay_text = Fdimer_prediction + 0.65 # Offset
-    plt.text(inlay_text, min(R2), f'Predicted FD = {Fdimer_prediction}', color='red', horizontalalignment='left')
+    plt.semilogy(fd, r2)
+    plt.axvline(x=fd_pred, color='red', linestyle='--')
+    inlay_text = fd_pred + 0.65 # Offset
+    plt.text(inlay_text, min(r2), f'Predicted FD = {fd_pred}', color='red', horizontalalignment='left')
     plt.xlabel('Dimer Fraction (FD)')
     plt.ylabel('Sum of Squared Residuals (R2)')
     plt.title("Least-Squares Analysis")
     plt.show()
 
 def fit_frac_dimer(pe, pm, pd):
-    """ 
+    """
     This function is made to calculate the fraction of dimer
     by minimization of the sum of squared residuals between simulated
-    ideal monomers and dimers 
+    ideal monomers and dimers
     Adapted from Chadda & Robertson JGP 2017 MatLab app.
-    
+
     pe, pm, pd: each is a list of 3 probabilities in order. [p1, p2, p3+]
     pe = Experimentally determined probabilities
     pm = Simulated probabilities for noninteracting monomers
     pd = Simulated probabilities for noninteracting dimers
     """
     # Create an array of dimer fractions (0 to 1) with a step of 0.01
-    FD = np.arange(0, 1.01, 0.01)
-    nFD = FD.shape  # Get the size of the FD array
-    
-    N = 3  # number of data points
-    
+    fd_array = np.arange(0, 1.01, 0.01)
+    n_fd = fd_array.shape  # Get the size of the FD array
+
+    N = 3  # number of probabilities
+
     # Calculation of the sum of squared residuals (R2)
-    R2 = []
-    for i in range(nFD[0]):
+    r2 = []
+    for i in range(n_fd[0]):
         total_sum = 0
         for k in range(N):
-            Pfit = (1 - FD[i]) * pm[k] + FD[i] * pd[k]
-            total_sum += (pe[k] - Pfit) ** 2
-        R2.append(total_sum)
-    
+            pfit = (1 - fd_array[i]) * pm[k] + fd_array[i] * pd[k]
+            total_sum += (pe[k] - pfit) ** 2
+        r2.append(total_sum)
+
     # Plotting the results
-    plt.semilogy(FD, R2)
+    plt.semilogy(fd_array, r2)
     plt.xlabel('Dimer Fraction (FD)')
     plt.ylabel('Sum of Squared Residuals (R2)')
     plt.title('Least-Squares Analysis')
     plt.show()
-    
+
     # Minimum R2 value and Fdimer prediction
-    min_R2 = min(R2)
-    min_R2_index = R2.index(min_R2)
-    Fdimer_prediction = FD[min_R2_index]
-    
-    least_squares_plot(FD, R2, Fdimer_prediction)
-    
-    print(f"Minimum R2 value: {min_R2}")
-    print(f"Fdimer prediction: {Fdimer_prediction}")
+    min_r2 = min(r2)
+    min_r2_index = r2.index(min_r2)
+    fd_pred = fd_array[min_r2_index]
+
+    least_squares_plot(fd_array, r2, fd_pred)
+
+    print(f"Minimum R2 value: {min_r2}")
+    print(f"Fdimer prediction: {fd_pred}")
+
+    pfit = calc_pfit(pe, pm, pd, fd_pred)
+
+    chi_squared(pe, pfit)
+
+def calc_pfit(pe, pm, pd, fd_pred):
+    """
+    Calculated the probabilities from the fitted fraction of dimer.
+    Used for chi-squared analysis.
+    """
+
+    # Plot resulting pfit
+    pfit = [(1 - fd_pred) * pm[0] + fd_pred * pd[0],
+            (1 - fd_pred) * pm[1] + fd_pred * pd[1],
+            (1 - fd_pred) * pm[2] + fd_pred * pd[2]]
+
+    plot_probability_bar(pe, title="$p_{exp}$")
+
+    plot_probability_bar(pfit, title="$p_{fit}$")
+
+    return pfit
+
+def chi_squared(pe, pfit):
+    # Chi-squared analysis
+    # H0: the expt and fitted distributions are the same
+    # H1: the expt and fitted distributions are different
+    chi2_value = 0
+    n = 3 # number of probabilities
+    for k in range(n):
+        chi2_value += ((pe[k] - pfit[k]) ** 2) / pfit[k]
+
+    # Calculate the p-value using the chi-squared distribution
+    p_value = 1 - chi2.cdf(chi2_value, n - 1)
+
+    # Replace the following lines with your actual output code (e.g., GUI updates or print statements)
+    print(f"Chi-squared Value: {chi2_value}")
+    print(f"P-value: {p_value}")
+
+    # Check if p-value is greater than 0.05
+    if p_value > 0.05:
+        print("The distributions are statistically different")
+    else:
+        print("The distributions are statistically the same")
+
 
 
 
 def main():
 
-   
+
     # List of p1, p2, p3+ from experimental data
     # No value for peptide concentration 0.09375e-6
-    pe = [[0.024451410660000002, 0.05141065831, 0.02413793103], 
-          [0.04580645161000001, 0.041612903230000005, 0.012580645159999999], 
-          [0.061494252869999995, 0.0316091954, 0.0068965517240000005], 
-          [0.0686, 0.0264, 0.005], 
+    pe = [[0.024451410660000002, 0.05141065831, 0.02413793103],
+          [0.04580645161000001, 0.041612903230000005, 0.012580645159999999],
+          [0.061494252869999995, 0.0316091954, 0.0068965517240000005],
+          [0.0686, 0.0264, 0.005],
           [0.07292225201, 0.023056300270000003, 0.004021447721]]
-    
+
     # These values were generated from sim_occ_over_pls
     # List of p1, p2, p3+ for noninteracting monomers
-    pm = [[0.3151, 0.5246, 0.27280000000000004], 
-          [0.3509, 0.3151, 0.0701], 
-          [0.3097, 0.1929, 0.022699999999999998], 
-          [0.2701, 0.1506, 0.0145], 
+    pm = [[0.3151, 0.5246, 0.27280000000000004],
+          [0.3509, 0.3151, 0.0701],
+          [0.3097, 0.1929, 0.022699999999999998],
+          [0.2701, 0.1506, 0.0145],
           [0.1729, 0.0664, 0.0021],
           [0.0965, 0.0296, 0.0003]]
-    
+
     # List of p1, p2, p3+ for noninteracting dimers
     pd = [[0.0004, 0.0006, 1.997999999999991],
           [0.0009, 0.0023, 1.994299999999997],
@@ -108,53 +154,3 @@ if __name__ == "__main__":
     main()
 
 
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from scipy.stats import chi2
-
-# # Assuming you have the following data
-# N = 3  # Replace with the actual number of data points
-# PMX = np.random.rand(N)  # Replace with your actual monomer probability data
-# PDX = np.random.rand(N)  # Replace with your actual dimer probability data
-# Pexpt = np.random.rand(N)  # Replace with your actual experimental data
-
-# # Define FD, I, and X (if not already defined)
-# FD = 0.5  # Replace with the desired FD value
-# I = 1  # Replace with the desired index
-# X = np.arange(1, N + 1)
-
-# # Plot resulting Pfit
-# Pfit = [(1 - FD) * PMX[0] + FD * PDX[0], (1 - FD) * PMX[1] + FD * PDX[1], (1 - FD) * PMX[2] + FD * PDX[2]
-
-# # Outputs Pfit
-# # Replace the following lines with your actual output code (e.g., GUI updates or print statements)
-# print(f"Pfit1 Value: {Pfit[0]}")
-# print(f"Pfit2 Value: {Pfit[1]}")
-# print(f"Pfit3 Value: {Pfit[2]}")
-
-# # Plot Pfit
-# plt.bar(X, Pfit)
-# plt.xlabel('X')
-# plt.ylabel('Pfit')
-# plt.title('Pfit Distribution')
-# plt.show()
-
-# # Chi-squared analysis
-# # H0: the expt and fitted distributions are the same
-# # H1: the expt and fitted distributions are different
-# chi2_value = 0
-# for k in range(N):
-#     chi2_value += ((Pexpt[k] - Pfit[k]) ** 2) / Pfit[k]
-
-# # Calculate the p-value using the chi-squared distribution
-# p_value = 1 - chi2.cdf(chi2_value, N - 1)
-
-# # Replace the following lines with your actual output code (e.g., GUI updates or print statements)
-# print(f"Chi-squared Value: {chi2_value}")
-# print(f"P-value: {p_value}")
-
-# # Check if p-value is greater than 0.05
-# if p_value > 0.05:
-#     print("The distributions are different")
-# else:
-#     print("The distributions are the same")
--- plot_probs.py
+++ plot_probs.py
@@ -12,31 +12,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_probability_bar(y_values, title=None):
-    """ 
+    """
     p is a list of 3 probabilities in the order of p1, p2, p3+
+    TODO: Add values inside or outside the bar, depending on the value.
     """
     # Check if there are exactly 3 y values
     if len(y_values) != 3:
         raise ValueError("Input list must contain exactly 3 y values.")
-    
+
     # Labels for the bars
-    labels = ["p1", "p2", "p3+"]
-    
+    category_labels = ["$p_1$", "$p_2$", "$p_{3+}$"]
+
     # X-axis positions for the bars
-    x = range(len(labels))
-    
+    x = range(len(category_labels))
+
     # Create the bar chart
-    plt.bar(x, y_values, tick_label=labels, color="skyblue", edgecolor="black")
+    plt.bar(x, y_values, tick_label=category_labels, color="skyblue", edgecolor="black")
     plt.ylabel("Probability")
-    
+
     if title:
         plt.title(title)
-    
+
+    for i, v in enumerate(y_values):
+        if v > 0.9*max(y_values):
+            plt.text(i, v - 0.02*max(y_values), f'{v:.2f}', ha='center', va='top', fontsize=12)  # Adjust the position
+        else:
+            plt.text(i, v + 0.01*max(y_values), f'{v:.2f}', ha='center', va='bottom', fontsize=12)  # Default position
+
     plt.show()
-    
-    
+
+
 def plot_probability_curves(pl, p1, p2, p3, title=None):
-    """ 
+    """
     Plots the p1, p2, and p3 curves for a simulation or experimental data.
     """
     if len(pl) != len(p1) != len(p2) != len(p3):
@@ -48,19 +55,19 @@ def plot_probability_curves(pl, p1, p2, p3, title=None):
 
     plt.xlabel("P/L")
     plt.ylabel("Probability")
-    
+
     if title:
         plt.title(title)
-    
+
     plt.legend()
     plt.show()
-    
+
 def plot_probability_comparison(pl, pe, pm, pd, title=None):
     """
-    Plots a comparison of one probability among the experimental data and the 
+    Plots a comparison of one probability among the experimental data and the
     monomer and dimer simulations.
     """
-    
+
     if len(pl) != len(pe) != len(pm) != len(pd):
         raise ValueError("Input lists must have the same length.")
 
@@ -70,25 +77,25 @@ def plot_probability_comparison(pl, pe, pm, pd, title=None):
 
     # Fill between the lines
     plt.fill_between(pl, pd, color='black')
-    plt.fill_between(pl, pd, pm, color='lightgray')    
+    plt.fill_between(pl, pd, pm, color='lightgray')
 
     plt.xlabel("P/L")
     plt.ylabel("Probability")
-    
+
     if title:
         plt.title(title)
-    
+
     plt.legend()
     plt.show()
-    
+
 def main():
-    
+
     # Example usage
     pm = [0.024451410660000002, 0.05141065831, 0.02413793103]
     plot_probability_bar(pm, "Monomer, P/L = 0.02")
-    
-    
-    
+
+
+
     # Example usage
     peptide_concentrations = np.array([1.5, 0.75, 0.46875, 0.375, 0.1875, 0.09375])*1e-6
     lipid_concentration = 75e-6
@@ -96,22 +103,22 @@ def main():
     pe1=[0.024451410660000002, 0.04580645161000001, 0.061494252869999995, 0.0686, 0.07292225201]
     pe2=[0.05141065831, 0.041612903230000005, 0.0316091954, 0.0264, 0.023056300270000003]
     pe3=[0.02413793103, 0.012580645159999999, 0.0068965517240000005, 0.005, 0.004021447721]
-    
-    # Trim the 6th value because it's not in the experimental data. 
+
+    # Trim the 6th value because it's not in the experimental data.
     pl, pe1, pe2, pe3 = pl[:5], pe1[:5], pe2[:5], pe3[:5]
-    
+
     plot_probability_curves(pl, pe1, pe2, pe3, title="Experimental")
-    
-    
-    
+
+
+
     # Example usage
     p1e = [0.024451410660000002, 0.04580645161000001, 0.061494252869999995, 0.0686, 0.07292225201]
     p1m = [0.3151, 0.3509, 0.3097, 0.2701, 0.1729, 0.0965]
     p1d = [0.0004, 0.0009, 0.0025, 0.0036, 0.0179, 0.1128]
-    
-    # Trim the 6th value because it's not in the experimental data. 
+
+    # Trim the 6th value because it's not in the experimental data.
     pl, p1e, p1m, p1d = pl[:5], p1e[:5], p1m[:5], p1d[:5]
-    
+
     plot_probability_comparison(pl, p1e, p1m, p1d, title="p1")
 
 if __name__ == "__main__":
